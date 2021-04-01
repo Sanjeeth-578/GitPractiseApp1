21ece2bd3fb13ce28d0374776325ca13d8a9cba2
sanjeeth <sanjeeth_ms@yahoo.com>
2021-04-01 19:33:29 +0530

    First Commit for GitPractiseApp1 Project with GitTest1 class

---
+++ src/com/sanjeeth/GitTest1.java
@@ -0,0 +1,10 @@
+package com.sanjeeth;
+
+public class GitTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("HELLO WORLD");
+	}
+
+}
