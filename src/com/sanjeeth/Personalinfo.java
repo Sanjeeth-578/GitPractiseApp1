abc57338f62e41952593c79ffad623b97489482d
sanjeeth <sanjeeth_ms@yahoo.com>
2021-04-04 18:52:20 +0530

    Hello from git added in Personalinfo file

--- src/com/sanjeeth/Personalinfo.java
+++ src/com/sanjeeth/Personalinfo.java
@@ -1,7 +1,7 @@
 package com.sanjeeth;
 
 public class Personalinfo {
-	
+
 	private int age;
 	private String name;
 	private String address;
@@ -11,9 +11,9 @@ public class Personalinfo {
 		this.name = name;
 		this.address = address;
 	}
-	
+
 	public void getInfo() {
-		
+		System.out.println("Hello World from Git");
 		System.out.println("Age: " + age);
 		System.out.println("Name: " + name);
 		System.out.println("Address: " + address);
